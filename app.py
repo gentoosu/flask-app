cf1e8ac51e01c5d2e9c790ddc3dfa0ac7622ea07
Mike Hershberger <mike.hershberger@gmail.com>
2025-02-11 15:02:47 -0500

    fix linting

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 products = ["Laptop", "Smartphone", "Headphones", "Smartwatch", "Tablet"]
 
+
 @app.route('/recommend')
 def recommend_product():
     return random.choice(products)
@@ -12,4 +13,3 @@ def recommend_product():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
