e18c195ca0ff8d37c14baac4240d8335936514ab
Mike Hershberger <mike.hershberger@gmail.com>
2025-02-11 15:01:48 -0500

    fix flake linting issues

--- app.py
+++ app.py
@@ -3,12 +3,13 @@ import random
 
 app = Flask(__name__)
 
-products = ["Laptop", "Smartphone", "Headphones", "Smartwatch",
-"Tablet"]
+products = ["Laptop", "Smartphone", "Headphones", "Smartwatch", "Tablet"]
 
 @app.route('/recommend')
 def recommend_product():
     return random.choice(products)
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
